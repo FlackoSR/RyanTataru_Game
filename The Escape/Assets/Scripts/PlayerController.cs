262726e66991857096bcd0b4b2dc114e2db29854
FlackoSR <B00129803@mytudublin.ie>
2020-11-27 17:42:46 +0000

    Added PlayerController Script/Made player jump up

---
+++ The Escape/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody robotRIGBODY;
+    public float jumpP = 10;
+    public float gravityModifier;
+    public bool isOnground = true;
+
+    void Start()
+    {
+        robotRIGBODY = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space) && isOnground)
+        {
+            robotRIGBODY.AddForce(Vector3.up * jumpP, ForceMode.Impulse);
+            isOnground = false;
+        }
+       
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnground = true;
+    }
+}
